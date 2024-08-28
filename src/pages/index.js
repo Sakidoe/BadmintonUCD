6da496836df20978efa6b7e7b1660a9516159ad8
Richard Huang <richardhuang@Richards-MacBook-Pro-7.local>
2024-08-28 16:44:26 -0700

    attempt to fix banner with custom domain host

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,29 @@ import { StaticImage } from 'gatsby-plugin-image'
 import AnimatedText from '../components/mainpage/AnimatedText'
 import './index.css'
 import { Link } from 'gatsby'
+import bannerImage from '../images/bucd_formal_funny_copy.jpg';
+
+
+const bannerStyle = {
+  backgroundImage: `url(${bannerImage})`,
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  height: '80vh',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center',
+  opacity: 0.9,
+};
+
+
 const IndexPage = () => {
   return (
     <div className="mainpage-controls">
       <Layout pageTitle>
-          <div className='front-page-banner'>
+          <div className='front-page-banner' style={bannerStyle}>
             <div class="typewriter">
               <h1>Badminton @ UC Davis</h1>
             </div>
